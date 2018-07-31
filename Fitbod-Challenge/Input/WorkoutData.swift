b32861bf4d2bdcbc5a2ddc1fa358723bafe9675c
Ambuj Punn <ambujpunn@gmail.com>
2018-07-30 17:37:42 -0700

    workout data file added, keep working on it

---
+++ Fitbod-Challenge/Input/WorkoutData.swift
@@ -0,0 +1,31 @@
+//
+//  WorkoutData.swift
+//  Fitbod-Challenge
+//
+//  Created by Ambuj Punn on 7/30/18.
+//  Copyright © 2018 Ambuj Punn. All rights reserved.
+//
+
+import Foundation
+
+struct ExerciseSet {
+    let name: String
+    let date: Date
+    let reps: Int
+    let weight: Float
+}
+
+// Possibly make this Singleton
+struct WorkoutData {
+    private let fileName: String
+    
+    init(_ csvFileName: String) {
+        fileName = csvFileName
+    }
+    
+    // MARK - Private
+    
+    func convertCSVFile(_ fileName: String) -> String {
+        
+    }
+}
